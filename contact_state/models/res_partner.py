c644fed15f3a4441149a9b9e4d57b5ad7942bf64
Shahil Rasheed <shahilrasheed6@gmail.com>
2025-10-31 10:12:37 +0530

    30/10/25

--- contact_state/models/res_partner.py
+++ contact_state/models/res_partner.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class ResPartner(models.Model):
@@ -9,17 +9,16 @@ class ResPartner(models.Model):
 
     state = fields.Selection(selection=[('customer','Customer'),
                                         ('vendor','Vendor'),
-                                        ('both','Both')],compute='_compute_state',store=True)
+                                        ('both','Both')],compute='_compute_state')
 
-    @api.depends('vat')
     def _compute_state(self):
-        print("ABC")
+        """Set the state of partners"""
         for rec in self:
-            if rec.sale_order_ids and rec.supplier_rank:
+            if rec.customer_rank and rec.supplier_rank:
                 rec.write({'state': 'both'})
             elif rec.supllier_rank:
                 rec.write({'state': 'vendor'})
-            elif rec.sale_order_ids:
+            elif rec.customer_rank:
                 rec.write({'state': 'customer'})
             else:
                 rec.write({'state':None})
