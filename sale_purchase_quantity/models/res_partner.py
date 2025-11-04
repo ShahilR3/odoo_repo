71367ddb8f8d48f5e8fabfdbb5a4eca9b76d06b2
Shahil Rasheed <shahilrasheed6@gmail.com>
2025-11-04 11:55:53 +0530

    3/11/25

--- sale_purchase_quantity/models/res_partner.py
+++ sale_purchase_quantity/models/res_partner.py
@@ -7,16 +7,16 @@ from odoo.fields import Command
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    # product_id = fields.Many2one('product.product','Products')
-    # sale_order_line_ids = fields.One2many('sale.order.line', 'partner_order_id',
-    #                                       compute='compute_sale_order_line_ids', store=True)
-    # purchase_order_line_ids = fields.One2many('purchase.order.line','partner_order_id',
-    #                                           compute='compute_purchase_order_line_ids', store=True)
-    # sale_quantity = fields.Float("Total Sale quantity", compute="compute_sale_quantity")
-    # purchase_quantity = fields.Float("Total Purchase Quantity", compute="_compute_purchase_quantity")
-    # tax_ids = fields.Many2many('account.tax', string='Taxes')
-    # price = fields.Float("Price")
-    # tax_price = fields.Float("Tax Price",compute='compute_tax')
+    product_id = fields.Many2one('product.product','Products')
+    sale_order_line_ids = fields.One2many('sale.order.line', 'partner_order_id',
+                                          compute='compute_sale_order_line_ids', store=True)
+    purchase_order_line_ids = fields.One2many('purchase.order.line','partner_order_id',
+                                              compute='compute_purchase_order_line_ids', store=True)
+    sale_quantity = fields.Float("Total Sale quantity", compute="compute_sale_quantity")
+    purchase_quantity = fields.Float("Total Purchase Quantity", compute="_compute_purchase_quantity")
+    tax_ids = fields.Many2many('account.tax', string='Taxes')
+    price = fields.Float("Price")
+    tax_price = fields.Float("Tax Price",compute='compute_tax')
     qty = fields.Float("Quantity")
     uom_id = fields.Many2one('uom.uom', string='Source UoM', required=True)
     uom_category_id = fields.Many2one('uom.category', string='UoM Category', related='uom_id.category_id',
@@ -26,40 +26,40 @@ class ResPartner(models.Model):
 
     @api.depends('qty', 'uom_id', 'target_uom_id')
     def _compute_converted_qty(self):
+        """Computing the quantity conversion"""
         for rec in self:
             if rec.qty and rec.uom_id and rec.target_uom_id:
                 rec.computed = rec.uom_id._compute_quantity(rec.qty, rec.target_uom_id)
             else:
                 rec.computed = 0.0
 
-    # @api.depends('product_id')
-    # def compute_sale_order_line_ids(self):
-    #     for rec in self:
-    #         if rec.sale_order_ids and rec.product_id:
-    #             rec.sale_order_line_ids = [Command.set(rec.sale_order_ids.mapped('order_line').filtered(lambda l: l.product_id == rec.product_id).ids)]
-    #         else:
-    #             rec.product_id = None
-    #
-    # @api.depends('product_id')
-    # def compute_purchase_order_line_ids(self):
-    #     for rec in self:
-    #         if rec.purchase_line_ids and rec.product_id:
-    #             rec.purchase_order_line_ids = [Command.set(rec.purchase_line_ids.filtered(lambda l: l.product_id == rec.product_id).ids)]
-    #         else:
-    #             rec.product_id = None
-    #
-    # def compute_sale_quantity(self):
-    #     """Sale order line Quantity"""
-    #     for partner in self:
-    #         sale_order_line = partner.sale_order_ids.mapped('order_line')
-    #         partner.write({'sale_quantity':sum(sale_order_line.mapped('product_uom_qty'))})
-    #
-    # def _compute_purchase_quantity(self):
-    #     """Purchase order line quantity"""
-    #     for rec in self:
-    #         rec.write({'purchase_quantity': sum(rec.purchase_line_ids.mapped('product_qty'))})
-    #
-    # @api.depends('tax_ids','price')
-    # def compute_tax(self):
-    #     for rec in self:
-    #         rec.write({'tax_price': rec.tax_ids.compute_all(rec.price)['total_included']})
+    @api.depends('product_id')
+    def compute_sale_order_line_ids(self):
+        """Computing the Sale order lines for that product"""
+        for rec in self:
+            if rec.sale_order_ids and rec.product_id:
+                rec.sale_order_line_ids = [Command.set(rec.sale_order_ids.mapped('order_line').filtered(lambda l: l.product_id == rec.product_id).ids)]
+
+    @api.depends('product_id')
+    def compute_purchase_order_line_ids(self):
+        """Computing the purchase order line for that product"""
+        for rec in self:
+            if rec.purchase_line_ids and rec.product_id:
+                rec.purchase_order_line_ids = [Command.set(rec.purchase_line_ids.filtered(lambda l: l.product_id == rec.product_id).ids)]
+
+    def compute_sale_quantity(self):
+        """Sale order line Quantity"""
+        for partner in self:
+            sale_order_line = partner.sale_order_ids.mapped('order_line')
+            partner.write({'sale_quantity':sum(sale_order_line.mapped('product_uom_qty'))})
+
+    def _compute_purchase_quantity(self):
+        """Purchase order line quantity"""
+        for rec in self:
+            rec.write({'purchase_quantity': sum(rec.purchase_line_ids.mapped('product_qty'))})
+
+    @api.depends('tax_ids','price')
+    def compute_tax(self):
+        """Computing the tax"""
+        for rec in self:
+            rec.write({'tax_price': rec.tax_ids.compute_all(rec.price)['total_included']})
--- server_action_change/models/sale_order.py
+++ server_action_change/models/sale_order.py
@@ -3,15 +3,15 @@
 from odoo import models
 
 
-class SaleState(models.Model):
+class SaleOrder(models.Model):
     """New states for delivery status"""
     _inherit = "sale.order"
 
     def action_unit_to_dozen(self):
+        """Changing the uom of draft sale"""
+        active_ids = self.env.context.get('active_ids', [])
         dozen_uom = self.env['uom.uom'].search([('name', '=', 'Dozens')], limit=1)
-        sale_order_lines = self.env['sale.order.line'].search([
-            ('product_uom.name', '=', 'Units'),
-            ('state', '=', 'draft')
-        ])
-        if sale_order_lines:
-            sale_order_lines.write({'product_uom': dozen_uom.id})
+        unit_uom = self.env['uom.uom'].search([('name', '=', 'Units')], limit=1)
+        sale_order_unit = self.env['sale.order'].browse(active_ids).mapped('order_line').filtered(lambda l:l.product_uom.id == unit_uom.id)
+        if sale_order_unit:
+            sale_order_unit.write({'product_uom': dozen_uom.id})
